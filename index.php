5b8721cc8efe3807e775be8db650ac4260083ee5
iamjoy <14xz6@queensu.ca>
2018-07-22 20:25:32 -0400

    Added ViewersController class. Moved the code from index page to the class

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
 <?php
 
-//--------------------------------------
-//Viewers' controller
-//--------------------------------------
+include_once "View/ViewersController.php";
 
-include_once "View/PlayersViewerHTML.php";
-include_once "View/PlayersViewerCLI.php";
-
-if( php_sapi_name() === 'cli' )
-{
-	$aPlayersViewerCLI = new PlayersViewerCLI();
-	$aPlayersViewerCLI->display();
-}
-else {
-	$aPlayersViewerHTML = new PlayersViewerHTML();
-	$aPlayersViewerHTML->display();
-}
+$aViewersController = new ViewersController();
+$aViewersController->display();
 
 ?>
